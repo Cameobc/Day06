7148645fa89a578fe826178f70ec11c7b9775bc9
Cameobc <dkqtk@naver.com>
2019-03-27 14:01:48 +0900

    test

--- src/com/google/Restaurant/FoodMenu.java
+++ src/com/google/Restaurant/FoodMenu.java
@@ -9,36 +9,18 @@ public class FoodMenu {
 		Food [] foods = null;	
 		FoodView view = new FoodView();
 		boolean check=true;
-		
+
 		foods = fi.makeFood();
 		while(check) {
 			System.out.println("1.주 문");
 			System.out.println("2.결 산");
 			int select = sc.nextInt();
-
 			if(select == 1) {
-				while(check) {
-					int num =0;
-					Food [] count = new Food[num];
-					for(int i =0; i<foods.length; i++) {
-						Food food = foods[i];
-						System.out.println((i+1)+"번 메뉴 "+food.menu);
-						System.out.println("가격 "+food.price);
-						System.out.println("재고수량 "+food.total);
-						System.out.println("=============================");
-					}//for end
-					System.out.println((foods.length+1)+"번. 주문");
-					System.out.println("메뉴를 선택하세요.");
-					select = sc.nextInt();			
-					System.out.println("수랑을 입력하세요.");
-					num = sc.nextInt();			
-					if(select!=(foods.length+1)) {
-						
-					}
-				}
-	
-				}
-		}//while
+				view.listview(foods);
+				
+
+			}
+		}//while1
 	}
 
 }
--- src/com/google/Restaurant/FoodView.java
+++ src/com/google/Restaurant/FoodView.java
@@ -4,33 +4,39 @@ import java.util.Scanner;
 
 public class FoodView {
 
-	public void listview(Food[] foods) {
-		
-		
-//		boolean check= true;
-//		Scanner sc = new Scanner(System.in);
-//		
-//		while(check) {
-//			int select =0;
-//			int num =0;
-//			Food [] count = new Food[num];
-//
-//			for(int i =0; i<foods.length; i++) {
-//				Food food = foods[i];
-//				System.out.println((i+1)+"번 메뉴 "+food.menu);
-//				System.out.println("가격 "+food.price);
-//				System.out.println("재고수량 "+food.total);
-//				System.out.println("=============================");
-//			}
-//			System.out.println((foods.length+1)+"번. 주문");
-//			System.out.println("메뉴를 선택하세요.");
-//			select = sc.nextInt();			
-//			System.out.println("수랑을 입력하세요.");
-//			num = sc.nextInt();			
-//			if(select!=(foods.length+1)) {
+	public void listview(Food [] foods) {			
+		boolean check= true;
+		Scanner sc = new Scanner(System.in);	
 
-//			}if
-//		}while
+		while(check) {
+			int select =0;
+			int num =0;
+			Food [] count = new Food[num];
+			for(int i =0; i<foods.length; i++) {
+				Food food = foods[i];
+				System.out.println((i+1)+"번 메뉴 "+food.menu);
+				System.out.println("가격 "+food.price);
+				System.out.println("재고수량 "+food.total);
+				System.out.println("=============================");
+			}
+			System.out.println((foods.length+1)+"번. 주문");
+			System.out.println("메뉴를 선택하세요.");
+			select = sc.nextInt();			
+			System.out.println("수랑을 입력하세요.");
+			num = sc.nextInt();
+			if(select==(foods.length+1)) { 
+				break;
+			}
+		}//while
+		for(int i =0; i<foods.length; i++) {
+			Food food = foods[i];
+			System.out.println(food.menu);
+			System.out.println(food.price);
+			System.out.println(food.total);
+			System.out.println(food.count);
+			System.out.println("=============================");
+
+		}	
 
 	}
 
